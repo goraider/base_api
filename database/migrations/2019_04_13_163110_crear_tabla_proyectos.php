6585b4f58e2f4ffd5704384d601d8a725d65577e
gorider <alejandro_gosain@hotmail.com>
2019-04-15 12:11:45 -0500

    Correccion migracion Proyectos

--- database/migrations/2019_04_13_163110_crear_tabla_proyectos.php
+++ database/migrations/2019_04_13_163110_crear_tabla_proyectos.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CrearTablaProyecto extends Migration
+class CrearTablaProyectos extends Migration
 {
     /**
      * Run the migrations.
